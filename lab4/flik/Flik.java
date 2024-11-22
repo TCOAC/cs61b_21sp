ec36763efb1f9f6f47dcc6d58e5a20a65c602513
YuanXin <3259699399@qq.com>
2024-11-22 23:46:32 +0800

    Update Flik.java
    reason

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -10,4 +10,11 @@ public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
         return a.equals(b);
     }
+    /**
+     * Java 为了优化内存使用，对于某些范围的数值，会在装箱时缓存这些对象。
+     * Integer 的默认缓存范围是 -128 到 127。
+     * 当装箱的值在这个范围内时，Java 会返回缓存中的同一个对象。
+     * 超出这个范围时，Java 会创建新的对象
+     * == 用于比较两个对象时，比较的是它们的引用是否相同（是否指向同一个内存地址），而不是值是否相等。
+    */
 }
